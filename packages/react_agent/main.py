3e987aa5d0d494c8404dd50299aa603311d6bc90
190578 <190578@D2031501>
2025-08-21 00:41:30 +0900

    hello world

--- packages/react_agent/main.py
+++ packages/react_agent/main.py
@@ -4,3 +4,12 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+class Client:
+
+    def __init__(self, model_name: str = "llama3.3"):
+        self.model_name = model_name
+
+        Ollama
+
+    def startChat:
